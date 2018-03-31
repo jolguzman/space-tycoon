337cddfc107a162508fedd9523b351b1b64d8bb5
Jose <joseluisga92@gmail.com>
2018-03-31 14:16:38 -0500

    firstcommit

---
+++ MarsOne/Assets/Assets/Scripts/Core/Grid.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+    // a SpriteRenderer that will be instantiated in a grid to create our board
+    public Transform m_emptySprite;
+    public bool m_display_grid;
+    public int m_grid_segment_length;
+    public int m_grid_segment_width;
+    public int m_length;
+    public int m_width;
+    
+    private int grid_x_position;
+    private int grid_z_position;
+	// Use this for initialization
+	void Start () {
+        grid_x_position = (int)this.transform.position.x;                   // gets te current x position of the grid
+        grid_z_position = (int)this.transform.position.z;                   // gets the current z position of the grid
+        draw_grid();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+
+    // draws a grid outline on the plane
+    private void draw_grid()
+    {
+		if (m_emptySprite)
+		{
+			for (int x = grid_x_position; x != grid_x_position - m_length ; x--)
+			{
+				for (int z = grid_z_position; z != grid_z_position - m_width; z--) 
+				{
+					Transform clone;
+                    clone = Instantiate(m_emptySprite, new Vector3(x , 0, z), Quaternion.identity) as Transform;
+
+					// names the empty squares for organizational purposes
+					clone.name = "Board Space ( x = " + x.ToString() +  " , z =" + z.ToString() + " )";
+                    // rotate empty square flat
+                    clone.Rotate(90, 0, 0);
+					// parents all of the empty squares to the Board object
+					clone.transform.parent = transform;
+				}
+			}
+		}
+	}
+
+}
---
+++ MarsOne/Assets/Assets/Scripts/Manager/Menu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public Button m_ShelterBtn;
+    public Button m_WaterBtn;
+    public Button m_ElectricityBtn;
+    public Button m_expandBtn;
+   
+	// Use this for initialization
+	void Start () {
+        Button btn = m_ShelterBtn.GetComponent<Button>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+	}
+
+    // will retrive buildable items given the criteria selected
+    void LoadItems()
+    {
+
+    }
+    void TaskOnClick()
+    {
+        Debug.Log("you clicked on Shelter");
+    }
+}
